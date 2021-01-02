ce5eff8c9ccba9cf8aebbf4ce950a6cc31ff0286
Abhinav Niranjan <66178038+knackofabhinav@users.noreply.github.com>
2021-01-03 03:22:36 +0530

    learning react basics

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello My Name is Abhinav
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [
+        {
+          name: 'John',
+          id: 'adfsdf'
+        },
+        {
+          name: 'Gea',
+          id: 'adffdsdf'
+        },
+        {
+          name: 'Johdfdfn',
+          id: 'adfsdfdfd'
+        }
+      ]
+    };
+  }
+
+  render() {
+    return(
+      <div className="App">
+        {this.state.monsters.map(monsters => (
+          <h1 key={monsters.id}> {monsters.name} </h1>
+        ))}
+      </div>
+    )
+  }
 }
 
 export default App;
